83eb4596aad3197d3bc4e2c67acac6528fb99131
New Name <prakhar.sharma190@gmail.com>
2017-05-05 01:21:03 +0530

    Configured device

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :feedbacks
   has_many :services, through: :booked_services
 end
