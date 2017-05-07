dd79d040db763d2e1f3c04f9e57bbd049a33c3ff
New Name <prakhar.sharma190@gmail.com>
2017-05-07 10:05:07 +0530

    added some content on admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -5,29 +5,32 @@ ActiveAdmin.register_page "Dashboard" do
   content title: proc{ I18n.t("active_admin.dashboard") } do
     div class: "blank_slate_container", id: "dashboard_default_message" do
       span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+        span ("Welcome to Gharpe Services Admin panel.")
+        small ("This is default admin dashboard.")
       end
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
+       column do
+          panel "Total Users" do
+           div class: 'stats count' do
+             User.count
+           end
+         end
+          panel "Total Products" do
+            div class: 'products count' do
+              Product.count
+            end
+          end
+          panel "Total Services" do
+            div class: 'service count' do
+              Service.count
+            end
+          end
+       end
+   end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+      
+     
   end # content
 end
